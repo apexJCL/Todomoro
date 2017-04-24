ae6834d5bd8f87de0adb14e9d4d40778c0329470
José Carlos López <jcl.isc@gmail.com>
2017-04-23 23:21:25 -0500

    unstable

--- app/src/main/java/me/apexjcl/todomoro/fragments/tabs/OverviewFragment.java
+++ app/src/main/java/me/apexjcl/todomoro/fragments/tabs/OverviewFragment.java
@@ -2,10 +2,16 @@ package me.apexjcl.todomoro.fragments.tabs;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.konifar.fab_transformation.FabTransformation;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.apexjcl.todomoro.R;
 
@@ -14,11 +20,18 @@ import me.apexjcl.todomoro.R;
  */
 public class OverviewFragment extends Fragment {
 
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton mFab;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_overview, container, false);
         ButterKnife.bind(this, v);
+        FabTransformation.with(mFab).transformTo(mToolbar);
         return v;
     }
 }
