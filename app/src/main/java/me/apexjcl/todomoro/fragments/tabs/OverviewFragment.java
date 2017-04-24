af78f603abc964aca3731b27cd999e62bfeb8813
José Carlos López <jcl.isc@gmail.com>
2017-04-23 23:44:49 -0500

    faboptions

--- app/src/main/java/me/apexjcl/todomoro/fragments/tabs/OverviewFragment.java
+++ app/src/main/java/me/apexjcl/todomoro/fragments/tabs/OverviewFragment.java
@@ -2,14 +2,12 @@ package me.apexjcl.todomoro.fragments.tabs;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.konifar.fab_transformation.FabTransformation;
+import com.joaquimley.faboptions.FabOptions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,18 +18,15 @@ import me.apexjcl.todomoro.R;
  */
 public class OverviewFragment extends Fragment {
 
-    @BindView(R.id.floatingActionButton)
-    FloatingActionButton mFab;
-
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
+    @BindView(R.id.fab_options)
+    FabOptions mFab;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_overview, container, false);
         ButterKnife.bind(this, v);
-        FabTransformation.with(mFab).transformTo(mToolbar);
+        mFab.setButtonsMenu(R.menu.fab_overview );
         return v;
     }
 }
