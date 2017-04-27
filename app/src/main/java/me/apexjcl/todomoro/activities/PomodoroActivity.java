a9546d956196cfed897e8820cd15e08029e0401b
José Carlos López <jcl.isc@gmail.com>
2017-04-27 17:05:53 -0500

    clean state

--- app/src/main/java/me/apexjcl/todomoro/activities/PomodoroActivity.java
+++ app/src/main/java/me/apexjcl/todomoro/activities/PomodoroActivity.java
@@ -4,13 +4,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.github.krtkush.lineartimer.LinearTimer;
 import io.github.krtkush.lineartimer.LinearTimerView;
 import me.apexjcl.todomoro.R;
 import me.apexjcl.todomoro.logic.Pomodoro;
@@ -18,7 +16,7 @@ import me.apexjcl.todomoro.realm.handlers.TaskHandler;
 import me.apexjcl.todomoro.realm.models.Task;
 
 
-public class PomodoroActivity extends AppCompatActivity implements LinearTimer.TimerListener {
+public class PomodoroActivity extends AppCompatActivity {
 
     @BindView(R.id.control_button)
     ImageView mControlButton;
@@ -26,8 +24,6 @@ public class PomodoroActivity extends AppCompatActivity implements LinearTimer.T
     LinearTimerView mTimerView;
 
     public static final String TASK_ID = "task_id";
-
-    private LinearTimer mTimer;
     private Pomodoro mPomodoro;
     private String mTaskId;
     private Task mTask;
@@ -51,38 +47,12 @@ public class PomodoroActivity extends AppCompatActivity implements LinearTimer.T
     private void init() {
         mTask = (Task) TaskHandler.getTask(mTaskId);
         mPomodoro = new Pomodoro(mTask);
-        initTimer();
-    }
-
-    private void initTimer() {
-        mTimer = new LinearTimer.Builder()
-                .linearTimerView(mTimerView)
-                .duration(30_000)
-                .timerListener(this)
-                .getCountUpdate(LinearTimer.COUNT_DOWN_TIMER, 500)
-                .build();
     }
 
 
     @OnClick(R.id.control_button)
     void controlTimer() {
-        switch (mTimer.getState()) {
-            case INITIALIZED:
-                setPauseButton();
-                mTimer.startTimer();
-                break;
-            case ACTIVE:
-                setPlayButton();
-                mTimer.pauseTimer();
-                break;
-            case PAUSED:
-                setPauseButton();
-                mTimer.resumeTimer();
-                break;
-            case FINISHED:
-                break;
-        }
-        Log.d("timer", mTimer.getState().name());
+
     }
 
 
@@ -106,24 +76,7 @@ public class PomodoroActivity extends AppCompatActivity implements LinearTimer.T
     protected void onDestroy() {
         mTask = null;
         mPomodoro = null;
-        mTimer.pauseTimer();
-        mTimer = null;
         super.onDestroy();
-    }
-
-    @Override
-    public void animationComplete() {
-        Log.d("timer", "animation complete");
-    }
-
-    @Override
-    public void timerTick(long tickUpdateInMillis) {
-        Log.d("timer", "Tick " + String.valueOf(tickUpdateInMillis));
-    }
-
-    @Override
-    public void onTimerReset() {
-        Log.d("timer", "reset");
     }
 
 }
